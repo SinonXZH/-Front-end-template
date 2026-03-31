2d04d32168fba6ebfb3a8a0064bddd32394da197
SinonXZH <104661342@qq.com>
2026-03-31 08:24:37 +0800

    修复eslint不能使用的问题

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,9 @@
-//eslint.config.mjs
-
 import js from "@eslint/js";
 import globals from "globals";
-import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   {
     files: ['**/*.js'],
     languageOptions: {
